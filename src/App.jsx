da4a9f4535084ab55f013f618d436ea98c1a0f37
Ishank307 <ishankkumar307@gmail.com>
2025-09-01 19:10:35 +0530

    port issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ const API_BASE_URL = 'http://localhost:8000/api';
 
 
 const api=axios.create({
-  baseURL:'',
-  // baseURL:'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '', 
   withCredentials:true,
 })
 
